bb6d02f7708c1f8bcfbda1685925ddd31482252d
yonnie-park <juon0805@gmail.com>
2026-03-12 17:39:31 +0900

    Initial Commit

---
+++ fitflex/fitflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitflexApp.swift
+//  fitflex
+//
+//  Created by Jessie Park on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct fitflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
